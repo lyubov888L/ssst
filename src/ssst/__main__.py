8b722f7a69641567d4e8e50f1424a51ca26085f3
Kyle Altendorf <sda@fstab.net>
2021-01-07 16:36:59 -0500

    some comments and issue links

--- src/ssst/__main__.py
+++ src/ssst/__main__.py
@@ -1,5 +1,6 @@
 # TODO: This is generally actually covered by ssst._tests.test_cli.test_gui_persists
-# but the coverage recording or reporting isn't working out.
+#       but the coverage recording or reporting isn't working out.
+#       See https://github.com/altendky/ssst/issues/13.
 
 import ssst.cli  # pragma: no cover
 
