b23d5f6d7b14f3da4493a0e91c811e64b595a051
Kyle Altendorf <sda@fstab.net>
2021-02-07 21:52:30 -0500

    and include the conversion script

---
+++ .github/resolve_anchors.py
@@ -0,0 +1,35 @@
+import pathlib
+
+import yaml
+
+
+def main():
+    here = pathlib.Path(__file__).parent
+    input_file = here.joinpath("ci.yml")
+    output_file = here.joinpath("workflows", "ci.yml")
+
+    loaded = yaml.load(input_file.read_text(encoding="UTF-8"), Loader=yaml.SafeLoader)
+    loaded.pop("anchors")
+    output_file.write_text(yaml.dump(loaded, Dumper=yaml.SafeDumper), encoding="UTF-8")
+
+
+main()
+
+# s = """
+# anchors:
+#     wifi_parm: &wifi_params
+#         ssid: 1
+#         key: 2
+# test1:
+#   name: connectivity
+#   <<: *wifi_params
+# test2:
+#   name: connectivity_5ghz
+#   <<: *wifi_params
+# """
+#
+# print(s)
+# data = yaml.load(s, Loader=yaml.SafeLoader)
+# print(data)
+# data.pop("anchors")
+# print(yaml.dump(data, Dumper=yaml.SafeDumper))
