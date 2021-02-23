5355dc315f5b83d58aa4b917dec57aed7e18c832
Gordon Schmidt <gordon.schmidt@epcpower.com>
2021-02-22 17:43:58 -0800

    build: insert path to import versioneer

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
 import setuptools
+import os
+import sys
+# Must insert directory of setup.py to path in order to import local file versioneer because of PEP 517.
+sys.path.insert(0, os.path.dirname(__file__))
 import versioneer
 
 setuptools.setup(
